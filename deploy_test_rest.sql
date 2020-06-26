d6d25b9e5eb450d3f625117da5202871544bf5b1
Pavel Mitrofanov <pavel@mitrofa.nov.ru>
2020-06-26 22:56:13 +0200

    Enable OAuth

--- deploy_test_rest.sql
+++ deploy_test_rest.sql
@@ -7,9 +7,22 @@
  */
 
 begin
-  ords.delete_module(
-    p_module_name => 'test_log'
-  );
+  for r in (select t.name from user_ords_modules t)
+  loop
+    ords.delete_module(r.name);
+  end loop;
+  for r in (select t.name from user_ords_privileges t where t.created_by = user)
+  loop
+    ords.delete_privilege(r.name);
+  end loop;
+  for r in (select t.name from user_ords_clients t)
+  loop
+    oauth.delete_client(r.name);
+  end loop;
+  for r in (select t.name from user_ords_roles t where t.created_by = user)
+  loop
+    ords.delete_role(r.name);
+  end loop;
   ords.define_module(
     p_module_name => 'test_log'
   , p_base_path   => 'log/'
@@ -106,6 +119,62 @@ begin
       end;
     '
   );
+  ords.create_role('ci');
+  declare
+    v_roles owa.vc_arr;
+    v_patterns owa.vc_arr;
+    v_modules owa.vc_arr;
+  begin
+    v_roles(1) := 'ci';
+    v_modules(1) := 'test_log';
+    ords.define_privilege(
+      p_privilege_name => 'test.log.privilege'
+    , p_roles          => v_roles
+    , p_patterns       => v_patterns
+    , p_modules        => v_modules
+    , p_label          => 'Read logs'
+    , p_description    => 'Allows accessing the DDL logs.'
+    , p_comments       => null
+    );
+  end;
+  ords.create_privilege_mapping(
+    p_privilege_name => 'test.log.privilege'
+  , p_pattern        => '/log/*');
+  declare
+    v_roles owa.vc_arr;
+    v_patterns owa.vc_arr;
+    v_modules owa.vc_arr;
+  begin
+    v_roles(1) := 'ci';
+    v_modules(1) := 'test_schema';
+    ords.define_privilege(
+      p_privilege_name => 'test.schema.privilege'
+    , p_roles          => v_roles
+    , p_patterns       => v_patterns
+    , p_modules        => v_modules
+    , p_label          => 'Manage schemas'
+    , p_description    => 'Allows creating and browsing test schemas.'
+    , p_comments       => null
+    );
+  end;
+  ords.create_privilege_mapping(
+    p_privilege_name => 'test.schema.privilege'
+  , p_pattern        => '/schema/*');
+  oauth.create_client(
+    p_name            => 'ci_application'
+  , p_grant_type      => 'client_credentials'
+  , p_owner           => 'CI Application'
+  , p_description     => 'Client for automated access from CI.'
+  , p_support_email   => 'example@example.com'
+  , p_privilege_names => null
+  , p_support_uri     => 'http://example.com/'
+  );
+  oauth.grant_client_role(
+    p_client_name => 'ci_application'
+  , p_role_name   => 'ci'
+  );
   commit;
 end;
 /
+
+select id, name, client_id, client_secret from user_ords_clients;
